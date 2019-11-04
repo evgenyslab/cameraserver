26c073941d3d852de44733ba28febf572590fd5c
Kevin Green <kevin.green@instacart.com>
2019-11-03 16:13:38 -0800

    Add CRUD

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -18,11 +18,25 @@ def hello_world():
     stream = requests.get('http://localhost:7777', stream=True)
 
     return Response(stream_with_context(stream.iter_content(chunk_size=8096)), content_type = stream.headers['content-type'])
-    
-@app.route('/settings/<path:setting>/<path:value>')
-def settings(setting, value):
 
+settings = {'example1': 'value', 'example2': 'value2'}
+
+@app.route('/settings')
+def list_settings():
+    return Response(settings)
+
+@app.route('/settings/<path:setting>')
+def get_setting(setting):
+    return Response(settings.get(setting))
+
+@app.route('/settings/<path:setting>/<path:value>',  methods=['GET', 'PUT', 'POST'])
+def set_setting(setting, value):
+    settings[setting] = value
     return f"You set {setting} and {value}"
+
+@app.route('/settings/<path:setting>', methods=['DELETE'])
+def delete_setting(setting):
+    return Response(settings.pop(setting, None))
 
 
 if __name__ == '__main__':
